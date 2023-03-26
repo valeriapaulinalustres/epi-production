3a969ea8a5f785a2bc72dbc049715eda5d5d4a0d
valeriapaulinalustres <valeriapaulinalustres@yahoo.com.ar>
2023-03-26 17:24:11 -0300

    import

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './app.css'
+import './App.css'
 
 // components
 import Home from './pages/home/Home';
