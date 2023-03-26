9c7e8e2dd0b136d57d22bed4906172e58b5e8fc5
valeriapaulinalustres <valeriapaulinalustres@yahoo.com.ar>
2023-03-26 10:40:58 -0300

    first

---
+++ back/index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+//rutas
+app.use('/api/users', (req,res)=>{
+    res.send('funciona')
+})
+
+
+
+app.listen('8083', () => {
+    console.log('Servidor escuchando en el puerto 8083');
+})
+
+
