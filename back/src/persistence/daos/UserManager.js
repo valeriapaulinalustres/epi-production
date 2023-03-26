aa4d8a2892be75d9484e87a9a5dc9b33da04ce4b
valeriapaulinalustres <valeriapaulinalustres@yahoo.com.ar>
2023-03-26 16:36:59 -0300

    src back

---
+++ back/src/persistence/daos/UserManager.js
@@ -0,0 +1,97 @@
+import { userModel } from '../models/users.models.js'
+import { hashPassword, comparePasswords } from '../../utils.js'
+
+
+export default class UsersManager {
+
+  async createUser(user) {
+ 
+    const { email, password } = user
+    try {
+      const existeUsuario = await userModel.find({ email })
+      if (existeUsuario.length === 0) {
+        const hashNewPassword = await hashPassword(password)
+        const newUser = { ...user, password: hashNewPassword }
+        await userModel.create(newUser)
+        return newUser
+      } else {
+        console.log('pasa por null')
+        //return null
+      }
+    } catch (error) {
+      console.log(error)
+      throw new Error(error)
+    }
+  }
+
+
+  async loginUser(user) {
+    console.log('aca', user)
+    try {
+      const { email, password } = user
+      const usuario = await userModel.findOne({ email })
+      if (usuario) {
+        const isPassword = comparePasswords(password, usuario.password)
+        if (isPassword) {
+          return usuario
+        }
+      }
+      return null
+    } catch (error) {
+      console.log(error)
+    }
+   
+  }
+
+
+async getUsers () {
+  try {
+    const users = await userModel.find()
+    if (users) { 
+      return users
+    } else {
+    return null
+    }
+    } catch (error) {
+      console.log(error)
+    }
+  
+  } 
+
+  async deleteUser (id) {
+    try {
+      const deletedUser = await userModel.findByIdAndDelete(id)
+      if (deletedUser) {
+        return deletedUser
+      } else {
+        return null
+      }
+    }  catch (error) {
+      console.log(error)
+  }
+}
+
+async editUser (id, editedUser) {
+  console.log(editedUser)
+   const { first_name, last_name, email, profession, job, rol} = editedUser
+  
+   try {
+    const editedUser = await userModel.findByIdAndUpdate(id, {
+      first_name, 
+      last_name,
+      email,
+      profession,
+      job,
+      rol
+    }, {new: true})
+    return editedUser
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+
+}
+
---
+++ back/src/persistence/routes/users.router.js
@@ -0,0 +1,85 @@
+import { Router } from "express";
+import UsersManager from "../daos/UserManager.js";
+
+const router = Router();
+const usersManager = new UsersManager();
+
+
+router.get("/", async (req, res) => {
+  //traerá la lista de usuarios y la mostrará en perfil
+  const users = await usersManager.getUsers();
+  if (users) {
+   // res.send('llega')
+    res.json(users);
+  } else {
+    res.json({ mensaje: "no hay usuarios en la base de datos" });
+  }
+});
+
+router.post("/register", async (req, res) => {
+  //desde el perfil, botón de agregar usuarios, registra usuarios nuevos
+  const newUser = await usersManager.createUser(req.body);
+  if (newUser) {
+    res.json({ mensaje: "usuario creado con éxito" });
+  } else {
+    res.json({ mensaje: "error al crear usuario" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  //login
+  const { email, password } = req.body;
+  console.log("ruta", req.body);
+
+  const user = await usersManager.loginUser(req.body);
+  if (user) {
+    const { first_name, last_name, profession, job, email, isAdmin } = user;
+    //  res.set('Access-Control-Allow-Origin', '*')
+    //    req.session.name = first_name
+    req.session.email = email;
+    req.session.password = password;
+    res.json({
+      user: { first_name, last_name, profession, job, email, isAdmin },
+    });
+  } else {
+    return null;
+    //   let mensaje = 'Usuario o contraseña inválidos'
+    // res.json({mensaje: mensaje})
+  }
+});
+
+router.put("/edit-user/:id", async (req, res) => {
+  //editará usuarios
+  const editedUser = await usersManager.editUser(req.params.id, req.body);
+  if (editedUser) {
+    return res.json({
+      mensaje: `Usuario ${editedUser.first_name} actualizado con éxito`,
+    });
+  } else {
+    res.json({ mensaje: "Error al editar usuario" });
+  }
+});
+
+router.delete("/delete-user/:id", async (req, res) => {
+  //eliminará un usuario
+
+  const deletedUser = await usersManager.deleteUser(req.params.id);
+  if (deletedUser) {
+    res.json({ mensaje: `usuario ${deletedUser} eliminado con éxito` });
+  } else {
+    res.json({ mensaje: "usuario no encontrado" });
+  }
+});
+
+router.get("/logout", async (req, res) => {
+  req.session.destroy((error) => {
+    if (error) {
+      console.log(error);
+      res.json({ mensaje: error });
+    } else {
+      res.json({ mensaje: "sesión eliminada" });
+    }
+  });
+});
+
+export default router;
