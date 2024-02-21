5d65e857660bd764d6549cce0a56a09c7cbf8a74
valeriapaulinalustres <valeriapaulinalustres@yahoo.com.ar>
2024-02-21 17:39:16 -0300

    add 2024 buttons

--- front/src/pages/upload/Upload.js
+++ front/src/pages/upload/Upload.js
@@ -7,8 +7,9 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Loading from '../../components/Loading/Loading';
 
-import excelFile from './moron2024.csv';
+import excelFile from './moron2022.csv';
 import excelFile2023 from './moron2023.csv';
+import excelFile2024 from './moron2024.csv';
 import excelFileClinica from './clinica2022.csv';
 import excelFileClinica2023 from './clinica2023.csv';
 import * as xlsx from 'xlsx';
@@ -255,6 +256,64 @@ function Upload() {
     requestClinica.send();
   }
 
+  //-----------función para cargar archivo local 2024-------------------------
+  function loadLocalFile2024() {
+    setSpinnerHome(true);
+    setSpinnerHomeClinica(true);
+
+    let json2023;
+
+    // get file from the imported url
+    let request2023 = new XMLHttpRequest();
+    request2023.open('GET', excelFile2024, true);
+    request2023.responseType = 'arraybuffer';
+    request2023.onload = function () {
+      /* convert data to binary string */
+      let data2023 = new Uint8Array(request2023.response);
+      let arr2023 = new Array();
+      for (var i = 0; i !== data2023.length; ++i)
+        arr2023[i] = String.fromCharCode(data2023[i]);
+      data2023 = arr2023.join('');
+
+      //using xlsx library convert file to json
+      const workbook = xlsx.read(data2023, { type: 'binary' });
+      const sheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[sheetName];
+      json2023 = xlsx.utils.sheet_to_json(worksheet);
+      //console.log(json)
+      setBaseCompleta(json2023);
+      setSpinnerHome(false);
+      setAnioBaseActual(2024);
+    };
+    request2023.send();
+
+    // //load clinica.csv file ***********************************
+
+    let jsonClinica;
+
+    // get file from the imported url
+    let requestClinica = new XMLHttpRequest();
+    requestClinica.open('GET', excelFileClinica2023, true);
+    requestClinica.responseType = 'arraybuffer';
+    requestClinica.onload = function () {
+      let dataClinica = new Uint8Array(requestClinica.response);
+      let arrClinica = new Array();
+      for (var i = 0; i !== dataClinica.length; ++i)
+        arrClinica[i] = String.fromCharCode(dataClinica[i]);
+      dataClinica = arrClinica.join('');
+
+      //using xlsx library convert file to json
+      const workbookClinica = xlsx.read(dataClinica, { type: 'binary' });
+      const sheetNameClinica = workbookClinica.SheetNames[0];
+      const worksheetClinica = workbookClinica.Sheets[sheetNameClinica];
+      jsonClinica = xlsx.utils.sheet_to_json(worksheetClinica);
+      //console.log(jsonClinica)
+      setBaseCompletaClinica(jsonClinica);
+      setSpinnerHomeClinica(false);
+    };
+    requestClinica.send();
+  }
+
   const semanasNumber = [];
   semanas.forEach(el => semanasNumber.push(parseInt(el)));
 
@@ -268,6 +327,9 @@ function Upload() {
         </button>
         <button onClick={loadLocalFile2023} className='buttonActive'>
           Cargar archivo local 2023
+        </button>
+        <button onClick={loadLocalFile2024} className='buttonActive'>
+          Cargar archivo local 202
         </button>
 
         {/* Input para cargar archivo csv externo */}
