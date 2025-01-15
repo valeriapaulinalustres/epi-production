ee47e461054c4b5c7275f9024573b2583023aee7
valeriapaulinalustres <valeriapaulinalustres@yahoo.com.ar>
2025-01-15 09:31:48 -0300

    upload december 2024 complete

--- front/src/pages/home/Home.js
+++ front/src/pages/home/Home.js
@@ -19,7 +19,7 @@ function Home() {
   return (
     <div className='home-container'>
       <h2>Vigilancia de Enfermedades de Notificación Obligatoria</h2>
-      <h6>Datos actualizados al 21/08/2024</h6>
+      <h6>Datos actualizados al 15/01/2025</h6>
       {/* <h6 style={{color: '#ff003c'}}>Se está trabajando en solucionar un error en Notificados por Morón de cada enfermedad</h6> */}
 
       {anioBaseActual && <h3>Datos del año {anioBaseActual}</h3>}
